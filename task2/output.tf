17283e9f478a4cad781939542443a7bc8dba5668
bincy041 <82860563+bincy041@users.noreply.github.com>
2021-05-06 19:30:37 +0530

    Update output.tf

--- task2/output.tf
+++ task2/output.tf
@@ -14,8 +14,3 @@ output"EXAMPLE"{
   value = local.exmp
 }
 
- #--value="${join("", local.Example[0])}"
- # value=[for example in local.Example : join("", local.Example[0])]
-# value=join(" " ,local.Example)
-#value=[for Example in local.Example : "${join("", Example)}"]
-
