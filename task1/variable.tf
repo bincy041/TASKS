666155e911377ac43522f060d1e5a406774578dc
bincy041 <82860563+bincy041@users.noreply.github.com>
2021-05-06 18:01:24 +0530

    Add files via upload

---
+++ task1/variable.tf
@@ -0,0 +1,33 @@
+
+
+variable "environment" {
+  default = "production"
+}
+ 
+variable "os" {
+  default = "windows"
+}
+ 
+variable "region" {
+  default = "us-west4"
+}
+ 
+variable "zone" {
+  default = "us-west4-a"
+}
+ 
+variable "serial-no" {
+  default = "0001"
+}
+ 
+variable "drive-mount" {
+  default = "data01"
+}
+ 
+variable "snapshot" {
+  default = "snp"
+}
+
+
+
+  
